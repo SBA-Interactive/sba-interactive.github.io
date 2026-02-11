9b7eb6a348a0a3819af205df3d2fa4ea730a0759
Bartosz Zakrzewski <bartoszzakrzewski@mti.edu.pl>
2026-02-11 19:59:41 +0100

    Update Removed deprecated API, some translations, Contact layout change

---
+++ _astro/svelte-component.QiMpmP2s.js
@@ -0,0 +1,1 @@
+import{k as s,l as c,o as m,E as i}from"./render.BLDbRzeb.js";import{B as p}from"./branches.CqPDLNbr.js";function l(n,r,o){s&&c();var e=new p(n);m(()=>{var a=r()??null;e.ensure(a,a&&(t=>o(t,a)))},i)}export{l as c};
